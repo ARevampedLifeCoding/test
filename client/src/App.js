e2bb45f0490ca6883b203911e02c3d9dcb4bedd1
Randy <83686830+ARevampedLifeCoding@users.noreply.github.com>
2021-11-30 14:48:57 -0500

    sending to ryan

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import './App.scss';
+import { useState } from 'react';
+import Navbar from './components/Navbar/Navbar'
+import Menu from './components/Menu/Menu'
+import Adopt from './components/Adopt/Adopt';
+import Login from './components/Auth/Auth'
+import Signup from "./components/Signup/Signup";
+
+function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  return (
+<div className="app">
+    <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+    <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+    <Menu />
+    <div className="sections">
+      <Adopt />
+      <Login />
+      <Signup />
+    </div>
+</div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Menu/Menu.js
@@ -0,0 +1,19 @@
+import './menu.scss'
+
+export default function Menu({ menuOpen, setMenuOpen }){
+    return (
+        <div className={'menu ' + (menuOpen && 'active')}>
+            <ul>
+                <li onClick={()=>setMenuOpen(false)}>
+                    <a href="#adopt">Adopt</a>
+                </li>
+                <li onClick={()=>setMenuOpen(false)}>
+                    <a href="#login">Log In</a>
+                </li>
+                <li onClick={()=>setMenuOpen(false)}>
+                    <a href="#signup">Sign In</a>
+                </li>
+            </ul>
+        </div>
+    )
+}
