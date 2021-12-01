b7988cd19d19999b1bad65351b4ea1ef086bcaca
Randy <83686830+ARevampedLifeCoding@users.noreply.github.com>
2021-12-01 11:40:47 -0500

    working on react hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,29 @@
-import './App.scss';
-import { useState } from 'react';
-import Navbar from './components/Navbar/Navbar'
-import Menu from './components/Menu/Menu'
-import Adopt from './components/Adopt/Adopt';
-import Login from './components/Auth/Auth'
+// import "./App.scss";
+import { Container } from "@material-ui/core";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import {  useState } from 'react';
+import { Provider } from "react-redux";
 
+// import Navbar from "./components/Navbar/Navbar";
+// import Menu from './components/Menu/Menu'
+import Adopt from "./components/Adopt/Adopt";
+// import Login from './components/Auth/Auth'
+import Auth from "./components/Auth/Auth";
+import Nav from "./components/Nav/Nav"
 
-function App() {
-  const [menuOpen, setMenuOpen] = useState(false);
-  return (
-<div className="app">
-    <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-    <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-    <Menu />
-    <div className="sections">
-      <Adopt />
-      <Login />
-      
-    </div>
-</div>
-  );
-}
+const App = () => {
+  <Provider>
+    <Router>
+      <Container maxWidth="lg">
+        {/* <Navbar /> */}
+        < Nav />
+        <Routes>
+          <Route path="/" exact component={Adopt} />
+          <Route path="/auth" exact component={Auth} />
+        </Routes>
+      </Container>
+    </Router>
+  </Provider>;
+};
 
 export default App;
