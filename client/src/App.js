6af69162a866744dbbc995ad77f7c26686478754
Ryan Barrett <ryan.barrett@medkeeper.com>
2021-12-02 17:33:01 -0700

    changed auth to use react context

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {createContext, useContext} from 'react';
 import {
   ApolloClient,
   InMemoryCache,
@@ -6,14 +6,12 @@ import {
   createHttpLink,
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { useState } from 'react';
 
-import Adopt from './components/Adopt/Adopt';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
 import Navbar from "./components/Navbar/Navbar"
 import Menu from './components/Menu/Menu'
+import AppRoutes from './routes/AppRoutes';
+import {AuthProvider} from './context/AuthContext'
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -34,29 +32,17 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
+
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
   return (
     <ApolloProvider client={client}>
-      <Router>
-        < Navbar />
-        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-    <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-    <Menu />
-        <div className="flex-column justify-flex-start min-100-vh">
-          <div className="container">
-            <Route exact path="/">
-              <Adopt />
-            </Route>
-            <Route exact path="/login">
-              <Login />
-            </Route>
-            <Route exact path="/signup">
-              <Signup />
-            </Route>
-          </div> 
-        </div>
-      </Router>
+      <AuthProvider>
+        <AppRoutes>
+            <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+            <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+        </AppRoutes>
+      </AuthProvider>
     </ApolloProvider>
   );
 }
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,6 +1,8 @@
 import './navbar.scss'
-
+import {useAuth} from '../../context/AuthContext';
 export default function Navbar({ menuOpen, setMenuOpen }){
+    const {isLoggedIn, currentUser, logout} = useAuth();
+    console.log(isLoggedIn, currentUser);
     return (
         <div className={"navbar " + (menuOpen && "active")}>
            <div className="wrapper">
@@ -9,6 +11,15 @@ export default function Navbar({ menuOpen, setMenuOpen }){
                 Furever Home <img src='./img/paw.gif' alt="Paw"></img>
                 </a>
             </div>
+            {isLoggedIn && 
+                <>
+                    <div>
+                        <span>logged in: {currentUser?.username}</span>
+                        {' '}
+                        <button onClick={logout}>logout</button>
+                    </div>
+                </>
+            } 
             <div className="right">
                 <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
                     <span className="line1"></span>
@@ -16,7 +27,7 @@ export default function Navbar({ menuOpen, setMenuOpen }){
                     <span className="line3"></span>
                 </div>
             </div>
-         </div>
+          </div>
         </div>
     )
 }
